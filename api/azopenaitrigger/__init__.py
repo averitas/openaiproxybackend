eb2db4a548bad542ab3476c0a060a9e878870693
Sicheng Liu <sichengliu@microsoft.com>
2023-08-09 19:29:59 +0800

    check user quote

--- api/azopenaitrigger/__init__.py
+++ api/azopenaitrigger/__init__.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import base64
 import json
 import logging
 import os
+from api.shared_lib.cache import RedisClientInst
+from api.shared_lib.cache.redis import QuoteState
+from api.shared_lib.types.models import UserInfo
 from shared_lib.db import PersistenceLayer, loadContextAsList
 from shared_lib.handler import Message, OpenaiHandler
 from azure.data.tables import TableServiceClient
@@ -14,22 +18,24 @@ app = func.FunctionApp()
 
 connectionString = os.getenv('AzureDataStorage', '')
 service = TableServiceClient.from_connection_string(conn_str=connectionString)
+db = PersistenceLayer()
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    db = PersistenceLayer()
     name = req.params.get('version')
-    
-    token = req.headers['Authorization'].lstrip("Bearer ")
-    logging.info('Receive auth token: ' + token)
-    logging.info(f'X-MS-CLIENT-PRINCIPAL-NAME is {req.headers.get("X-MS-CLIENT-PRINCIPAL-NAME", "N/A")}')
-    logging.info(f'X-MS-CLIENT-PRINCIPAL is {req.headers.get("X-MS-CLIENT-PRINCIPAL", "N/A")}')
-    
     if name:
         chatgpt_model_name = os.getenv("CHATGPT_MODEL")
         api_version = os.getenv('OPENAI_API_VERSION')
         return func.HttpResponse(f"Hello, {name}. Model: {chatgpt_model_name}, apiversion: {api_version}.")
+    
+    upn = getUserNameFromRequest(req)
+    passed = checkQuote(upn)
+    if not passed:
+        return func.HttpResponse(
+            f"API quote for user {upn} is exceeded, please contact admin.",
+            status_code=200
+        )
     
     try:
         req_body = req.get_json()
@@ -66,3 +72,44 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
              f"Error: {ex}",
              status_code=500
         )
+
+# User principle in header X-MS-CLIENT-PRINCIPAL format like:
+# {
+#     "auth_typ": "",
+#     "claims": [
+#         {
+#             "typ": "",
+#             "val": ""
+#         }
+#     ],
+#     "name_typ": "",
+#     "role_typ": ""
+# }
+# get typ is 'preferred_username' in 'claims'
+def getUserNameFromRequest(req: func.HttpRequest) -> str:
+    token = req.headers['Authorization'].split(' ')[-1]
+    logging.info('Receive auth token: ' + token)
+    userPrincipleBytes = base64.b64decode(req.headers.get("X-MS-CLIENT-PRINCIPAL"))
+    userClaims = json.loads(userPrincipleBytes.decode('utf8'), strict=False)
+    
+    # find preferred_username in claims
+    for claimsItem in userClaims['claims']:
+        if claimsItem['typ'] == 'preferred_username':
+            return claimsItem['val']
+    logging.warning("No user email found in token: " + userClaims)
+    raise ValueError("Couldn't find user email in token")
+
+def checkQuote(userId: str) -> bool:
+    state = RedisClientInst.CheckUserQuote(userId=userId)
+    if state == QuoteState.EXCEEDED:
+        return False
+    if state == QuoteState.NOTEXIST:
+        userInfo = db.retrieveUser(userId=userId)
+        if not userInfo:
+            userInfo = UserInfo(email=userId)
+            db.saveUser(user=userInfo)
+        RedisClientInst.SetUserQuote(userInfo.Email, userInfo.QuoteInDay)
+        return True
+    if state == QuoteState.OK:
+        return True
+    return False
